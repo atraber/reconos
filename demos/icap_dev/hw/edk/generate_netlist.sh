a0d86ea3095ae22eb45a7f52517c5df9a073d9ab
atraber <andytraber@gmail.com>
2014-03-21 16:03:34 +0100

    Copied icap_demo to icap_dev

---
+++ demos/icap_dev/hw/edk/generate_netlist.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+################################################################################
+#                                                                              #
+#                      CONFIGURATION A, ADDITION                               #
+#                                                                              #
+################################################################################
+echo "######################################################################"
+echo "BUILDING CONFIGURATION A"
+echo "######################################################################"
+
+# Make sure that we are really building the addition
+sed -i '67s/.*/\tresult1 <= val1 + val2;/' ./pcores/hwt_pr_block_v1_00_a/hdl/vhdl/hwt_pr_block.vhd
+
+################################################################################
+# Building netlist
+################################################################################
+echo "run netlist" | xps -nw ./system.xmp
+
+################################################################################
+# Move netlist to new folder where PAR is executed
+################################################################################
+cp ./implementation/*.ngc ../pr_design/syn/static/
+
+# Netlist of Reconfigurable Module is moved to a folder specific for this
+# configuration
+mv ../pr_design/syn/static/system_hwt_pr_block_0_wrapper.ngc ../pr_design/syn/pr_a/
+
+echo "CONFIGURATION A FINISHED"
+
+################################################################################
+#                                                                              #
+#                      CONFIGURATION B, SUBTRACTION                            #
+#                                                                              #
+################################################################################
+echo "######################################################################"
+echo "BUILDING CONFIGURATION B"
+echo "######################################################################"
+
+# Make sure that we are really building the subtraction
+sed -i '67s/.*/\tresult1 <= val1 - val2;/' ./pcores/hwt_pr_block_v1_00_a/hdl/vhdl/hwt_pr_block.vhd
+
+################################################################################
+# Building netlist
+################################################################################
+echo "run netlist" | xps -nw ./system.xmp
+
+################################################################################
+# Move netlist to new folder where PAR is executed
+################################################################################
+cp ./implementation/*.ngc ../pr_design/syn/static/
+
+# Netlist of Reconfigurable Module is moved to a folder specific for this
+# configuration
+mv ../pr_design/syn/static/system_hwt_pr_block_0_wrapper.ngc ../pr_design/syn/pr_b/
+
+echo "CONFIGURATION B FINISHED"
